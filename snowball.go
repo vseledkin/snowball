c9ee880a80567fde3c5642aaea597415b770fcea
viacheslav seledkin <vseledkin@ws24364.avicomp.com>
2017-05-29 17:52:58 +0300

    Make isStop public

--- snowball.go
+++ snowball.go
@@ -2,6 +2,7 @@ package snowball
 
 import (
 	"fmt"
+
 	"github.com/kljensen/snowball/english"
 	"github.com/kljensen/snowball/french"
 	"github.com/kljensen/snowball/russian"
@@ -20,6 +21,29 @@ func Stem(word, language string, stemStopWords bool) (stemmed string, err error)
 	switch language {
 	case "english":
 		f = english.Stem
+	case "spanish":
+		f = spanish.Stem
+	case "french":
+		f = french.Stem
+	case "russian":
+		f = russian.Stem
+	default:
+		err = fmt.Errorf("Unknown language: %s", language)
+		return
+	}
+	stemmed = f(word, stemStopWords)
+	return
+
+}
+
+// IsStop test if a word is stop wort in the specified language.
+//
+func IsStop(word, language string) (stemmed string, err error) {
+
+	var f func(string, bool) string
+	switch language {
+	case "english":
+		f = english.IsStop
 	case "spanish":
 		f = spanish.Stem
 	case "french":
